cbb82ec20aa4297d6af73fd3c0ed26fb581b8816
Gmandali <Gaurav.mandali@outlook.com>
2023-02-26 15:06:25 -0600

    xz

---
+++ pywork-master/relearningPython/variables.py
@@ -0,0 +1,25 @@
+# variable = a container for a value. Behaves as the value that it contains.
+# These are the types of variables and values in Python
+
+# String
+first_name = "Gaurav"
+last_name = "Mandali"
+full_name = first_name + " " + last_name
+print("Hello, " + full_name)
+print(type(full_name))
+
+# Integer
+age = 21
+age += 1
+print("Your age is: " + str(age))
+print(type(age))
+
+# Float
+height = 250.5
+print("Your height is: " + str(height) + " cm")
+print(type(height))
+
+# Boolean
+human = False
+print("Are you a human:  " + str(human))
+print(type(human))
