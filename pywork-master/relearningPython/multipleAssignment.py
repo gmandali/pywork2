47c26acd23a21b1c44cc01d3023bd2bdf797b92b
Gmandali <Gaurav.mandali@outlook.com>
2023-03-04 07:41:44 -0600

    xz

---
+++ pywork-master/relearningPython/multipleAssignment.py
@@ -0,0 +1,24 @@
+# multiple assignment = allows us to assign multiple variable at the same time in one line of code
+
+# name = "Gaurav"
+# age = 13
+# attractive = True
+
+name, age, attractive = "Gaurav", 13, True
+
+print(name)
+print(age)
+print(attractive)
+
+# If all the variables are the same
+# Spongebob = 30
+# Patrick = 30
+# Sandy = 30
+# Squidward = 30
+
+Spongebob = Patrick = Sandy = Squidward = 30
+
+print(Spongebob)
+print(Patrick)
+print(Sandy)
+print(Squidward)
