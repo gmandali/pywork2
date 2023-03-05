8e6d9b616c168d18fef88a5b603c7a48509960ab
Gmandali <Gaurav.mandali@outlook.com>
2023-03-05 08:44:06 -0600

    class

---
+++ pywork-master/pythonClass/WeightOfPeopleOnDifferentPlanets.py
@@ -0,0 +1,5 @@
+WoP = float(input("Weight of person here: "))
+SGE = 9.81
+WoPl = float(input("Gravity of Planet: "))
+FinalWeight = WoP / SGE * WoPl
+print("Weight on planet is:", FinalWeight)
---
+++ pywork-master/pythonClass/milesToKilometers.py
@@ -0,0 +1,4 @@
+Km = float(input("Number of Km: "))
+Mi = 0.62137
+numOfMiles = Km * Mi
+print(Km, "kilometers is equal to:", numOfMiles, "miles.")
